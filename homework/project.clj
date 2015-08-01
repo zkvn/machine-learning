773f740bebd4f900da893593be4fc4037f2dde6f
Kevin <kevin@kfs-MacBook-Pro.local>
2015-08-01 11:13:48 +0800

    update

---
+++ homework/project.clj
@@ -0,0 +1,9 @@
+(defproject homework "0.1.0-SNAPSHOT"
+  :description "Homework in clojure for mellon machine learning course"
+  :url "http://www.cs.cmu.edu/~tom/10701_sp11/lectures.shtml"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                  [incanter/incanter-core "1.5.6"]
+
+                 ])
